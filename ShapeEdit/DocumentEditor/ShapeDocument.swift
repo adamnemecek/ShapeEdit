c628bb7fcb9717fc77401875b413dded5d53ed2d
Adam Nemecek <adamnemecek@gmail.com>
2017-04-05 18:03:31 -0700

    * stuff

--- ShapeEdit/DocumentEditor/ShapeDocument.swift
+++ ShapeEdit/DocumentEditor/ShapeDocument.swift
@@ -9,39 +9,6 @@
 import UIKit
 import SceneKit
 
-extension SCNVector3  {
-    public func encode(with aCoder: NSCoder) {
-        aCoder.encode(x, forKey: "x")
-        aCoder.encode(y, forKey: "y")
-        aCoder.encode(z, forKey: "z")
-
-    }
-
-    public init?(coder aDecoder: NSCoder) {
-        x = aDecoder.decodeFloat(forKey: "x")
-        y = aDecoder.decodeFloat(forKey: "y")
-        z = aDecoder.decodeFloat(forKey: "z")
-    }
-}
-
-
-extension SCNVector4  {
-    public func encode(with aCoder: NSCoder) {
-        aCoder.encode(x, forKey: "rx")
-        aCoder.encode(y, forKey: "ry")
-        aCoder.encode(z, forKey: "rz")
-        aCoder.encode(w, forKey: "rw")
-        
-    }
-    
-    public init?(coder aDecoder: NSCoder) {
-        x = aDecoder.decodeFloat(forKey: "rx")
-        y = aDecoder.decodeFloat(forKey: "ry")
-        z = aDecoder.decodeFloat(forKey: "rz")
-        w = aDecoder.decodeFloat(forKey: "rw")
-    }
-}
-
 /// We save and restore the camera state as an object to be able to use `NSCoding`.
 class CameraState: NSObject, NSCoding {
     // MARK: - Properties
